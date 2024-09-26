25321683441c4ead95a220edaca3a64806231bf6
= <=>
2024-09-26 13:34:12 -0600

    Updating readme and turning in final assignment

--- recursive_traversal.py
+++ recursive_traversal.py
@@ -1,29 +1,30 @@
-def list_traverse(lst, depth=1, totalItems = 0):
+def list_traverse(lst, depth=-1, totalItems = 0):
     """ Recursively sum all the items in a list as well as it's nested lists
     Args:
         lst (list): A list to sum the items of
 
     Returns:
-        tuple: sum of the list
+        tuple: sum of the list, number of calls, number of items, deepest item
     """
     sum = 0
-    totalItems = 0
+    totalItems = len(lst)
     calls = 0
-    deepest= 0
-    recieved = len(lst)
+    deepest= depth+1
     for i in lst:
         if type(i) is int:
             sum += i
         elif type(i) is list:
-            recieved = list_traverse(i, depth+1, totalItems)
-            sum += recieved[0]
-            totalItems += recieved[2]
-            calls += recieved[1] + 1
-            print(recieved, i, deepest)
+            received = list_traverse(i, depth+1, totalItems)
+            sum += received[0]
+            totalItems += received[2]
+            calls += received[1] + 1
+            if deepest < received[-1]:
+                deepest = received[-1]
         else:
             print(f"Expected int, got {type(i)}: {i}")
-    return (sum, calls, recieved, deepest)
+    print(deepest)
+    return (sum, calls, totalItems, deepest)
 
 
-lst = [1, [2, 3], [[4], [5, [6, 7]]], 8]
+lst = [1, [2, 3], [[4], [5, [6, 7, [8, 9, 10, [11, [12]]]]]], 12]
 print(list_traverse(lst))
